fb6e19fd0236b784b750e6bd0ef1fb1c79aabc04
Sanmu <msanmugapriya18@gmail.com>
2024-10-14 20:06:40 +0530

    commit and push

---
+++ src/Oct/ex_09102024/Lab005.java
@@ -0,0 +1,11 @@
+package Oct.ex_09102024;
+
+public class Lab005 {
+    public static void main(String[] args) {
+        System.out.println("HELLO WORLD");
+        System.out.println("HELLO WORLD");
+        System.out.print("HELLO WORLD");
+        System.out.print("HELLO WORLD");
+
+    }
+}
